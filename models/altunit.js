4fc4c56067e565cbf035f8c0339594494d3893bf
PratimaGupta10 <78458523+PratimaGupta10@users.noreply.github.com>
2021-08-03 12:45:12 +0530

    changes

--- models/altunit.js
+++ models/altunit.js
@@ -1,30 +1,31 @@
-const mongoose = require("mongoose")
-const Schema = mongoose.Schema
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
 
-
-const altunitSchema = new Schema({
+const altunitSchema = new Schema(
+  {
     alt_unit_title: {
-        type: String,
-        require: true
+      type: String,
+      require: true,
     },
     value: {
-        type: Number,
-        require: true
+      type: Number,
+      require: true,
     },
     desc: {
-        type: String,
-        require: true
+      type: String,
+      require: true,
     },
     sortorder: {
-        type: String,
-        require: true
+      type: String,
+      require: true,
     },
     status: {
-        type: String,
-        default: "Active"
-    }
-},
-    { timestamps: true }
-)
+      type: String,
+      active: "Active",
+      deactive: "Inactive",
+    },
+  },
+  { timestamps: true }
+);
 
-module.exports = mongoose.model("altunit", altunitSchema)
+module.exports = mongoose.model("altunit", altunitSchema);
