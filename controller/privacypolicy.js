ffe7425bb346f5e9f3503c5e1e5cf49e605346ac
PratimaGupta10 <78458523+PratimaGupta10@users.noreply.github.com>
2021-08-25 15:45:19 +0530

    changes

--- controller/privacypolicy.js
+++ controller/privacypolicy.js
@@ -40,3 +40,20 @@ exports.delprivacypolicy = async (req, res) => {
     });
   }
 };
+
+exports.allprivacy_policy = async (req, res) => {
+  const findall = await privacypolicy.find().sort({ sortorder: 1 });
+  if (findall) {
+    res.status(200).json({
+      status: true,
+      msg: "success",
+      data: findall,
+    });
+  } else {
+    res.status(400).json({
+      status: false,
+      msg: "error",
+      error: "error",
+    });
+  }
+};
--- routes/privacypolicy.js
+++ routes/privacypolicy.js
@@ -4,12 +4,11 @@ const router = express.Router();
 const {
   addprivacypolicy,
   delprivacypolicy,
+  allprivacy_policy,
 } = require("../controller/privacypolicy");
 
 router.post("/admin/addprivacy_policy", addprivacypolicy);
-// router.post("/admin/editunits/:id", editunits);
-// router.get("/admin/viewoneunits/:id", viewoneunits);
-// router.get("/admin/viewallunits", viewallunits);
+router.get("/admin/allprivacy_policy", allprivacy_policy);
 router.get("/admin/delprivacypolicy/:id", delprivacypolicy);
 
 module.exports = router;
