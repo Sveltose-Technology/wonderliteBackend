706b3254a6b8a7d9a7cb0f8c2cd2b4f8ae696ba2
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2021-10-01 11:52:46 +0530

    hrhjfrd

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ require("dotenv").config();
 const mongoose = require("mongoose");
 const cors = require("cors");
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true })); //{ limit: "50mb" }
 app.use(cors());
 
 //require
