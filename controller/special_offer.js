e069a4114d8daac4aa33784ff3b8702470328ef8
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2021-10-17 23:26:58 +0530

    few chnage

--- controller/special_offer.js
+++ controller/special_offer.js
@@ -49,7 +49,7 @@ exports.add_specialoffer = async (req, res) => {
     sortorder: sortorder,
   });
   if (req.file) {
-    console.log(req.file);
+    // console.log(req.file);
     const findexist = await Specialoffer.findOne({
       specialoffer_title: specialoffer_title,
     });
