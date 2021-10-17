6a1a77a0b6bfa5a4809501d1b89ddc1d0989c3f6
pratimagupta98 <pratimadevelopersveltosest@gmail.com>
2021-10-17 15:09:31 +0530

    few changes

--- controller/flashsale.js
+++ controller/flashsale.js
@@ -49,7 +49,6 @@ exports.add_flashsale = async (req, res) => {
 
   if (req.files) {
     const findexist = await Flashsale.findOne({
-      //console.log(req.file)
       flashsale_title: flashsale_title,
     });
     if (findexist) {
